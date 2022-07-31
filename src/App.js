3a44475d135e8e71ef6325208ecb71bb9445d291
Hochan Lee <hochan.lee@riiid.co>
2022-07-31 16:41:58 +0900

    add bgm

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Invitaion from './component/Invitation';
 import Thanks from './component/Thanks';
 import { Route, Routes } from 'react-router-dom';
+import useSound from './hook/useSound';
+
 
 function App() {
+  useSound("./last_goodbye.mp3");
   return (
     <Routes>
       <Route path="/" element={<Invitaion />} />
